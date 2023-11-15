858bdbbf3fd584b2c3d17c0f0b62e6678b16988f
Skotnyj Dvor <80914910+skotnyjdvor@users.noreply.github.com>
2023-11-15 14:14:34 +0100

    Update default.sh

--- default.sh
+++ default.sh
@@ -22,10 +22,7 @@ EXTENSIONS=(
 )
 
 CHECKPOINT_MODELS=(
-    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
-    #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
-    "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
-    "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
+
 	"https://civitai.com/api/download/models/161185?type=Model&format=SafeTensor&size=full&fp=fp16"
 )
 
